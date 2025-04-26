87d6f9520f28b0a51ce12772eb393ccc8e75d27f
Şenol Yusuf Bağdu <yusufsenolbagdu@gmail.com>
2025-04-26 05:21:29 +0300

    sabah 5.30 arkaplan
    zaferimmmmm

---
+++ MIRAS/Assets/script giris ekranı/playGame.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class playGame : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public void playGamee()
+    {
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+
+    }
+
+    public void quickGame()
+    {
+
+        SceneManager.LoadScene("Main Menü");
+
+    }
+    public void settings()
+    {
+        SceneManager.LoadScene("Settings Menü");
+    }
+
+
+
+}
